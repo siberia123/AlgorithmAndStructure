131dbe761c3e95d2c928f64fa01f3e6d4a506a43
siberia <siberiacurrent@gmail.com>
2020-05-29 21:21:53 +0800

    commit all

---
+++ Sorting/quick_sort.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void quick_sort(){
+
+}
+
+int main(){
+
+    return 0;
+}
--- Sorting/selection_sort.c
+++ Sorting/selection_sort.c
@@ -8,7 +8,7 @@
 
 void selection_sort(int arr[],int size){
     //总共需要排序 size - 1 次，因为最后一个剩下的元素肯定是最小的（每次都会在未排序的元素中挑选最小的，放在已排序好元素的后端。）
-    for (int i = 0; i < size - 1; ++i) { //这个 i 充当着分解线的作用
+    for (int i = 0; i < size - 1; ++i) { //这个 i 充当着分界线的作用
         //用来记录最小元素
         int minIndex = i;
         //在未排序好元素中遍历，寻找最小元素
@@ -24,11 +24,11 @@ void selection_sort(int arr[],int size){
     }
 }
 
-int main(){
-    int arr[] = {3,2,5,4,64,23,45,34,46,423,34,4,3,343,53};
-    selection_sort(arr,15);
-    for (int i = 0; i < 15; ++i) {
-        printf("%d ",arr[i]);
-    }
-    return 0;
-}
+//int main(){
+//    int arr[] = {3,2,5,4,64,23,45,34,46,423,34,4,3,343,53};
+//    selection_sort(arr,15);
+//    for (int i = 0; i < 15; ++i) {
+//        printf("%d ",arr[i]);
+//    }
+//    return 0;
+//}
